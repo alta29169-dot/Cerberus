0b3e091fa8cd79220ef8736093cc64f8410c1530
alta29169-dot <alta29169@gmail.com>
2026-08-08 14:23:15 +0800

    Create services.lua

---
+++ harbor-defender/services.lua
@@ -0,0 +1,9 @@
+return {
+    Players = game:GetService("Players"),
+    Workspace = game:GetService("Workspace"),
+    RunService = game:GetService("RunService"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    UserInputService = game:GetService("UserInputService"),
+    LocalPlayer = game:GetService("Players").LocalPlayer,
+    Remote = game:GetService("ReplicatedStorage"):WaitForChild("Event")
+}
