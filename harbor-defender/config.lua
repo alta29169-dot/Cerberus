4180535a1a0d813bb88afe6899da897c84f679f7
alta29169-dot <alta29169@gmail.com>
2026-08-08 14:22:01 +0800

    Create config.lua

---
+++ harbor-defender/config.lua
@@ -0,0 +1,22 @@
+return {
+    HARBOR_RANGE = 1000,
+    FLOAT_HEIGHT = 200,
+    
+    -- Kill Aura
+    SHOTS_PER_BURST = 3,
+    SHOT_INTERVAL = 0.025,
+    BURST_COOLDOWN = 2.0,
+    KILL_RANGE = 50,
+    
+    -- Stockpile
+    STOCKPILE_DISTANCE = 15,
+    STOCKPILE_MAX = 2,
+    STOCKPILE_CHECK_INTERVAL = 0.5,
+    
+    -- Loopkill
+    TELEPORT_INTERVAL = 0.05,
+    
+    R6_PARTS = {
+        "Head", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg"
+    }
+}
