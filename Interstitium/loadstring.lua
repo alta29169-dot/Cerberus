7c4b44563895555773bca2d7d168127ccc97b0f0
alta29169-dot <alta29169@gmail.com>
2026-08-30 08:10:46 +0800

    Update loadstring.lua

--- Interstitium/loadstring.lua
+++ Interstitium/loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/alta29169-dot/Cerberus/refs/heads/main/Interstitium/Autowin.lua'))()
+https://raw.githubusercontent.com/alta29169-dot/Cerberus/refs/heads/main/Interstitium/PlayerB
